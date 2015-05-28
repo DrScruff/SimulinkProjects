7c537e9f517aa5453b19769176c9de3d3cd8ac17
DrScruff <mscaife@mac.com>
2015-05-28 23:25:11 +0100

    First Save

---
+++ work/InitialiseTelescope.m
@@ -0,0 +1,42 @@
+% Initialise Telescope Variables
+
+Body.Tube.Length     = 1320;     % mm
+Body.Tube.InnerRadius= 125;      % mm
+Body.Tube.Thickness  = 1;        % mm
+Body.Tube.RGB = [0.5 1 1];
+
+BT = Body.Tube;
+
+Body.Tube.XSection = [BT.InnerRadius 0; ...
+                      BT.InnerRadius + BT.Thickness 0; ...
+                      BT.InnerRadius + BT.Thickness BT.Length; ...
+                      BT.InnerRadius BT.Length];
+
+Body.Mirror.Radius   = 115;      % mm
+Body.Mirror.Depth    = 35;       % mm
+
+Body.Mirror.ZOffset  = 100 + Body.Mirror.Depth / 2; 
+
+Body.Shaft.Left.Length    = 100;       % mm
+Body.Shaft.Left.Radius    = 15;       % mm
+Body.Shaft.Left.RGB       = [1 1 1];  % White
+
+Body.Shaft.Left.XOffset = 0;
+Body.Shaft.Left.YOffset = Body.Tube.InnerRadius + Body.Shaft.Left.Length /2;
+Body.Shaft.Left.ZOffset = 430; % mm
+
+Body.Shaft.Left.Translation =  [Body.Shaft.Left.XOffset ...
+                                Body.Shaft.Left.YOffset ...
+                                Body.Shaft.Left.ZOffset];
+
+Body.Shaft.Right.Length    = 100;       % mm
+Body.Shaft.Right.Radius    = 15;       % mm
+Body.Shaft.Right.RGB       = [1 1 1];  % White
+                            
+Body.Shaft.Right.XOffset = 0;
+Body.Shaft.Right.YOffset = -1 * (Body.Tube.InnerRadius + Body.Shaft.Right.Length /2);
+Body.Shaft.Right.ZOffset = 430; % mm
+
+Body.Shaft.Right.Translation =  [Body.Shaft.Right.XOffset ...
+                                Body.Shaft.Right.YOffset ...
+                                Body.Shaft.Right.ZOffset];
